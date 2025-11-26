6e3aecbb0f76dc8dcf1244c4127c514edac9435d
francescoleonizera <92604601+francescoleonizera@users.noreply.github.com>
2025-11-26 15:28:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from glasspy.predict import GlassNet
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://www.submerged-combustion.com"])
 
 # Lazy load del modello
 glass_model = None
